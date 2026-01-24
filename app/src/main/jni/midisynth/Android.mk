53c4731eaaf84e73662a7feaa462ce9529056bc5
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2026-01-24 20:17:31 +0100

    sonivox integration using cmake

--- app/src/main/jni/midisynth/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := midisynth
-LOCAL_SRC_FILES := midisynth.c
-LOCAL_LDLIBS := -lOpenSLES -llog
-LOCAL_STATIC_LIBRARIES := libsonivox opensl_stream
-LOCAL_LDFLAGS := -Wl,--build-id=none
-include $(BUILD_SHARED_LIBRARY)
